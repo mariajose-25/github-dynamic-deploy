e22212da7c188bc7b639d3ebbca0ed10da8c7b0b
María José Rejala <83018673+mariajose-25@users.noreply.github.com>
2024-12-09 12:01:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("¡Hola, mundo! Esta es la versión inicial para pruebas de majo.");
+    res.send("¡Hola, mundo! Esta es la versión inicial para pruebas de fer.");
 });
 
 app.listen(port, () => {
